24aac2ff9c200260f2bcd6a1c452e8ef014f7a0a
JRnihar <niharmonirulislam@gmail.com>
2022-05-19 01:12:39 +0600

    update object id

--- index.js
+++ index.js
@@ -1,10 +1,53 @@
 const express = require('express');
 const app = express();
-// const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 const cors = require('cors');
 app.use(cors());
 app.use(express.json())
+
+
+//To-do-app
+
+//appuser
+//WjhvR3jI7k5DXHY4
+
+const uri = "mongodb+srv://appuser:WjhvR3jI7k5DXHY4@cluster0.dctmt.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        await client.connect();
+        const listCollection = client.db('To-do-app').collection('list');
+
+        app.get('/list', async (req, res) => {
+            const query = {};
+            const cursor = listCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+        app.post('/list', async (req, res) => {
+            const newService = req.body;
+            console.log(newService);
+            const result = await listCollection.insertOne(newService);
+            res.send(result);
+        });
+
+        app.delete('/list/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await listCollection.deleteOne(query);
+            res.send(result);
+        });
+
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
+
 
 
 app.get('/', (req, res) => {
